22485db4a811276b39f342c70d7f8f2428f415be
Miroslav Sergiev <miroslav.sergiev@egt-bg.com>
2018-03-28 12:26:28 +0300

    Added instruction consumption by length

--- DISASSEMBLER.h
+++ DISASSEMBLER.h
@@ -34,10 +34,26 @@ public:
             file.seekg( instNum );
             file.read( &val, 1 );
             if( file.eof() ) return;
+            unsigned char inst = (unsigned char)val;
             std::cout << std::hex << "0x" << std::setfill('0') << std::setw(4) << instNum;
-            std::cout << std::dec << " (" << instNum  << ")";
-            std::cout << " -> " << INST[(unsigned char)val] << std::endl;
-            instNum++;
+//             std::cout << std::dec << " (" << instNum  << ") -> ";
+            std::cout << ": ";
+            if( inst == 0xCB ) {
+                file.seekg( instNum+1 );
+                file.read( &val, 1 );
+                if( file.eof() ) return;
+                inst = (unsigned char)val;
+                std::cout << EXT_INST[inst];
+                instNum+=EXT_INST_LEN;
+            } else {
+                if( INST_LEN[inst] == 0 ) {
+                    std::cerr << "Instruction error" << std::endl;
+                    return;
+                }
+                std::cout << INST[inst];
+                instNum+=INST_LEN[inst];
+            }
+            std::cout << std::hex << " [" << +inst << "]" << std::endl;
         }
         
 	}
--- INSTRUCTION_SET.h
+++ INSTRUCTION_SET.h
@@ -263,4 +263,525 @@ static const std::string INST[] = {
     "RST 38H"             //0xFF
 };
 
+static const unsigned char INST_LEN[] = {
+    1,    //0x00
+    3,    //0x01
+    1,    //0x02
+    1,    //0x03
+    1,    //0x04
+    1,    //0x05
+    2,    //0x06
+    1,    //0x07
+    3,    //0x08
+    1,    //0x09
+    1,    //0x0A
+    1,    //0x0B
+    1,    //0x0C
+    1,    //0x0D
+    2,    //0x0E
+    1,    //0x0F
+    2,    //0x10
+    3,    //0x11
+    1,    //0x12
+    1,    //0x13
+    1,    //0x14
+    1,    //0x15
+    2,    //0x16
+    1,    //0x17
+    2,    //0x18
+    1,    //0x19
+    1,    //0x1A
+    1,    //0x1B
+    1,    //0x1C
+    1,    //0x1D
+    2,    //0x1E
+    1,    //0x1F
+    2,    //0x20
+    3,    //0x21
+    1,    //0x22
+    1,    //0x23
+    1,    //0x24
+    1,    //0x25
+    2,    //0x26
+    1,    //0x27
+    2,    //0x28
+    1,    //0x29
+    1,    //0x2A
+    1,    //0x2B
+    1,    //0x2C
+    1,    //0x2D
+    2,    //0x2E
+    1,    //0x2F
+    2,    //0x30
+    3,    //0x31
+    1,    //0x32
+    1,    //0x33
+    1,    //0x34
+    1,    //0x35
+    2,    //0x36
+    1,    //0x37
+    2,    //0x38
+    1,    //0x39
+    1,    //0x3A
+    1,    //0x3B
+    1,    //0x3C
+    1,    //0x3D
+    2,    //0x3E
+    1,    //0x3F
+    1,    //0x40
+    1,    //0x41
+    1,    //0x42
+    1,    //0x43
+    1,    //0x44
+    1,    //0x45
+    1,    //0x46
+    1,    //0x47
+    1,    //0x48
+    1,    //0x49
+    1,    //0x4A
+    1,    //0x4B
+    1,    //0x4C
+    1,    //0x4D
+    1,    //0x4E
+    1,    //0x4F
+    1,    //0x40
+    1,    //0x51
+    1,    //0x52
+    1,    //0x53
+    1,    //0x54
+    1,    //0x55
+    1,    //0x56
+    1,    //0x57
+    1,    //0x58
+    1,    //0x59
+    1,    //0x5A
+    1,    //0x5B
+    1,    //0x5C
+    1,    //0x5D
+    1,    //0x5E
+    1,    //0x5F
+    1,    //0x60
+    1,    //0x61
+    1,    //0x62
+    1,    //0x63
+    1,    //0x64
+    1,    //0x65
+    1,    //0x66
+    1,    //0x67
+    1,    //0x68
+    1,    //0x69
+    1,    //0x6A
+    1,    //0x6B
+    1,    //0x6C
+    1,    //0x6D
+    1,    //0x6E
+    1,    //0x6F
+    1,    //0x00
+    1,    //0x71
+    1,    //0x72
+    1,    //0x73
+    1,    //0x74
+    1,    //0x75
+    1,    //0x76
+    1,    //0x77
+    1,    //0x78
+    1,    //0x79
+    1,    //0x7A
+    1,    //0x7B
+    1,    //0x7C
+    1,    //0x7D
+    1,    //0x7E
+    1,    //0x7F
+    1,    //0x80
+    1,    //0x81
+    1,    //0x82
+    1,    //0x83
+    1,    //0x84
+    1,    //0x85
+    1,    //0x86
+    1,    //0x87
+    1,    //0x88
+    1,    //0x89
+    1,    //0x8A
+    1,    //0x8B
+    1,    //0x8C
+    1,    //0x8D
+    1,    //0x8E
+    1,    //0x8F
+    1,    //0x90
+    1,    //0x91
+    1,    //0x92
+    1,    //0x93
+    1,    //0x94
+    1,    //0x95
+    1,    //0x96
+    1,    //0x97
+    1,    //0x98
+    1,    //0x99
+    1,    //0x9A
+    1,    //0x9B
+    1,    //0x9C
+    1,    //0x9D
+    1,    //0x9E
+    1,    //0x9F
+    1,    //0xA0
+    1,    //0xA1
+    1,    //0xA2
+    1,    //0xA3
+    1,    //0xA4
+    1,    //0xA5
+    1,    //0xA6
+    1,    //0xA7
+    1,    //0xA8
+    1,    //0xA9
+    1,    //0xAA
+    1,    //0xAB
+    1,    //0xAC
+    1,    //0xAD
+    1,    //0xAE
+    1,    //0xAF
+    1,    //0x00
+    1,    //0xB1
+    1,    //0xB2
+    1,    //0xB3
+    1,    //0xB4
+    1,    //0xB5
+    1,    //0xB6
+    1,    //0xB7
+    1,    //0xB8
+    1,    //0xB9
+    1,    //0xBA
+    1,    //0xBB
+    1,    //0xBC
+    1,    //0xBD
+    1,    //0xBE
+    1,    //0xBF
+    1,    //0xC0
+    1,    //0xC1
+    3,    //0xC2
+    3,    //0xC3
+    3,    //0xC4
+    1,    //0xC5
+    2,    //0xC6
+    1,    //0xC7
+    1,    //0xC8
+    1,    //0xC9
+    3,    //0xCA
+    1,    //0xCB
+    3,    //0xCC
+    3,    //0xCD
+    2,    //0xCE
+    1,    //0xCF
+    1,    //0xD0
+    1,    //0xD1
+    3,    //0xD2
+    1,    //0xD3
+    3,    //0xD4
+    1,    //0xD5
+    2,    //0xD6
+    1,    //0xD7
+    1,    //0xD8
+    1,    //0xD9
+    3,    //0xDA
+    1,    //0xDB
+    3,    //0xDC
+    1,    //0xDD
+    2,    //0xDE
+    1,    //0xDF
+    2,    //0xE0
+    1,    //0xE1
+    2,    //0xE2
+    1,    //0xE3
+    1,    //0xE4
+    1,    //0xE5
+    2,    //0xE6
+    1,    //0xE7
+    2,    //0xE8
+    1,    //0xE9
+    3,    //0xEA
+    1,    //0xEB
+    1,    //0xEC
+    1,    //0xED
+    2,    //0xEE
+    1,    //0xEF
+    2,    //0xF0
+    1,    //0xF1
+    2,    //0xF2
+    1,    //0xF3
+    1,    //0xF4
+    1,    //0xF5
+    2,    //0xF6
+    1,    //0xF7
+    2,    //0xF8
+    1,    //0xF9
+    3,    //0xFA
+    1,    //0xFB
+    1,    //0xFC
+    1,    //0xFD
+    2,    //0xFE
+    1,    //0xFF
+};   
+
+
+static const std::string EXT_INST[] = {
+    "RLC B",        //0x00
+    "RLC C",        //0x01
+    "RLC D",        //0x02
+    "RLC E",        //0x03
+    "RLC H",        //0x04
+    "RLC L",        //0x05
+    "RLC (HL)",     //0x06
+    "RLC A",        //0x07
+    "RRC B",        //0x08
+    "RRC C",        //0x09
+    "RRC D",        //0x0A
+    "RRC E",        //0x0B
+    "RRC H",        //0x0C
+    "RRC L",        //0x0D
+    "RRC (HL)",     //0x0E
+    "RRC A",        //0x0F
+    "RL B",         //0x10
+    "RL C",         //0x11
+    "RL D",         //0x12
+    "RL E",         //0x13
+    "RL H",         //0x14
+    "RL L",         //0x15
+    "RL (HL)",      //0x16
+    "RL A",         //0x17
+    "RR B",         //0x18
+    "RR C",         //0x19
+    "RR D",         //0x1A
+    "RR E",         //0x1B
+    "RR H",         //0x1C
+    "RR L",         //0x1D
+    "RR (HL)",      //0x1E
+    "RR A",         //0x1F
+    "SLA B",        //0x20
+    "SLA C",        //0x21
+    "SLA D",        //0x22
+    "SLA E",        //0x23
+    "SLA H",        //0x24
+    "SLA L",        //0x25
+    "SLA (HL)",     //0x26
+    "SLA A",        //0x27
+    "SRA B",        //0x28
+    "SRA C",        //0x29
+    "SRA D",        //0x2A
+    "SRA E",        //0x2B
+    "SRA H",        //0x2C
+    "SRA L",        //0x2D
+    "SRA (HL)",     //0x2E
+    "SRA A",        //0x2F
+    "SWAP B",       //0x30
+    "SWAP C",       //0x31
+    "SWAP D",       //0x32
+    "SWAP E",       //0x33
+    "SWAP H",       //0x34
+    "SWAP L",       //0x35
+    "SWAP (HL)",    //0x36
+    "SWAP A",       //0x37
+    "SRL B",        //0x38
+    "SRL C",        //0x39
+    "SRL D",        //0x3A
+    "SRL E",        //0x3B
+    "SRL H",        //0x3C
+    "SRL L",        //0x3D
+    "SRL (HL)",     //0x3E
+    "SRL A",        //0x3F
+    "BIT 0,B",      //0x40
+    "BIT 0,C",      //0x41
+    "BIT 0,D",      //0x42
+    "BIT 0,E",      //0x43
+    "BIT 0,H",      //0x44
+    "BIT 0,L",      //0x45
+    "BIT 0,(HL)",   //0x46
+    "BIT 0,A",      //0x47
+    "BIT 1,B",      //0x48
+    "BIT 1,C",      //0x49
+    "BIT 1,D",      //0x4A
+    "BIT 1,E",      //0x4B
+    "BIT 1,H",      //0x4C
+    "BIT 1,L",      //0x4D
+    "BIT 1,(HL)",   //0x4E
+    "BIT 1,A",      //0x4F
+    "BIT 2,B",      //0x40
+    "BIT 2,C",      //0x51
+    "BIT 2,D",      //0x52
+    "BIT 2,E",      //0x53
+    "BIT 2,H",      //0x54
+    "BIT 2,L",      //0x55
+    "BIT 2,(HL)",   //0x56
+    "BIT 2,A",      //0x57
+    "BIT 3,B",      //0x58
+    "BIT 3,C",      //0x59
+    "BIT 3,D",      //0x5A
+    "BIT 3,E",      //0x5B
+    "BIT 3,H",      //0x5C
+    "BIT 3,L",      //0x5D
+    "BIT 3,(HL)",   //0x5E
+    "BIT 3,A",      //0x5F
+    "BIT 4,B",      //0x60
+    "BIT 4,C",      //0x61
+    "BIT 4,D",      //0x62
+    "BIT 4,E",      //0x63
+    "BIT 4,H",      //0x64
+    "BIT 4,L",      //0x65
+    "BIT 4,(HL)",   //0x66
+    "BIT 4,A",      //0x67
+    "BIT 5,B",      //0x68
+    "BIT 5,C",      //0x69
+    "BIT 5,D",      //0x6A
+    "BIT 5,E",      //0x6B
+    "BIT 5,H",      //0x6C
+    "BIT 5,L",      //0x6D
+    "BIT 5,(HL)",   //0x6E
+    "BIT 5,A",      //0x6F
+    "BIT 6,B",      //0x00
+    "BIT 6,C",      //0x71
+    "BIT 6,D",      //0x72
+    "BIT 6,E",      //0x73
+    "BIT 6,H",      //0x74
+    "BIT 6,L",      //0x75
+    "BIT 6,(HL)",   //0x76
+    "BIT 6,A",      //0x77
+    "BIT 7,B",      //0x78
+    "BIT 7,C",      //0x79
+    "BIT 7,D",      //0x7A
+    "BIT 7,E",      //0x7B
+    "BIT 7,H",      //0x7C
+    "BIT 7,L",      //0x7D
+    "BIT 7,(HL)",   //0x7E
+    "BIT 7,A",      //0x7F
+    "RES 0,B",      //0x80
+    "RES 0,C",      //0x81
+    "RES 0,D",      //0x82
+    "RES 0,E",      //0x83
+    "RES 0,H",      //0x84
+    "RES 0,L",      //0x85
+    "RES 0,(HL)",   //0x86
+    "RES 0,A",      //0x87
+    "RES 1,B",      //0x88
+    "RES 1,C",      //0x89
+    "RES 1,D",      //0x8A
+    "RES 1,E",      //0x8B
+    "RES 1,H",      //0x8C
+    "RES 1,L",      //0x8D
+    "RES 1,(HL)",   //0x8E
+    "RES 1,A",      //0x8F
+    "RES 2,B",      //0x90
+    "RES 2,C",      //0x91
+    "RES 2,D",      //0x92
+    "RES 2,E",      //0x93
+    "RES 2,H",      //0x94
+    "RES 2,L",      //0x95
+    "RES 2,(HL)",   //0x96
+    "RES 2,A",      //0x97
+    "RES 3,B",      //0x98
+    "RES 3,C",      //0x99
+    "RES 3,D",      //0x9A
+    "RES 3,E",      //0x9B
+    "RES 3,H",      //0x9C
+    "RES 3,L",      //0x9D
+    "RES 3,(HL)",   //0x9E
+    "RES 3,A",      //0x9F
+    "RES 4,B",      //0xA0
+    "RES 4,C",      //0xA1
+    "RES 4,D",      //0xA2
+    "RES 4,E",      //0xA3
+    "RES 4,H",      //0xA4
+    "RES 4,L",      //0xA5
+    "RES 4,(HL)",   //0xA6
+    "RES 4,A",      //0xA7
+    "RES 5,B",      //0xA8
+    "RES 5,C",      //0xA9
+    "RES 5,D",      //0xAA
+    "RES 5,E",      //0xAB
+    "RES 5,H",      //0xAC
+    "RES 5,L",      //0xAD
+    "RES 5,(HL)",   //0xAE
+    "RES 5,A",      //0xAF
+    "RES 6,B",      //0x00
+    "RES 6,C",      //0xB1
+    "RES 6,D",      //0xB2
+    "RES 6,E",      //0xB3
+    "RES 6,H",      //0xB4
+    "RES 6,L",      //0xB5
+    "RES 6,(HL)",   //0xB6
+    "RES 6,A",      //0xB7
+    "RES 7,B",      //0xB8
+    "RES 7,C",      //0xB9
+    "RES 7,D",      //0xBA
+    "RES 7,E",      //0xBB
+    "RES 7,H",      //0xBC
+    "RES 7,L",      //0xBD
+    "RES 7,(HL)",   //0xBE
+    "RES 7,A",      //0xBF
+    "SET 0,B",      //0xC0
+    "SET 0,C",      //0xC1
+    "SET 0,D",      //0xC2
+    "SET 0,E",      //0xC3
+    "SET 0,H",      //0xC4
+    "SET 0,L",      //0xC5
+    "SET 0,(HL)",   //0xC6
+    "SET 0,A",      //0xC7
+    "SET 1,B",      //0xC8
+    "SET 1,C",      //0xC9
+    "SET 1,D",      //0xCA
+    "SET 1,E",      //0xCB
+    "SET 1,H",      //0xCC
+    "SET 1,L",      //0xCD
+    "SET 1,(HL)",   //0xCE
+    "SET 1,A",      //0xCF
+    "SET 2,B",      //0xD0
+    "SET 2,C",      //0xD1
+    "SET 2,D",      //0xD2
+    "SET 2,E",      //0xD3
+    "SET 2,H",      //0xD4
+    "SET 2,L",      //0xD5
+    "SET 2,(HL)",   //0xD6
+    "SET 2,A",      //0xD7
+    "SET 3,B",      //0xD8
+    "SET 3,C",      //0xD9
+    "SET 3,D",      //0xDA
+    "SET 3,E",      //0xDB
+    "SET 3,H",      //0xDC
+    "SET 3,L",      //0xDD
+    "SET 3,(HL)",   //0xDE
+    "SET 3,A",      //0xDF
+    "SET 4,B",      //0xE0
+    "SET 4,C",      //0xE1
+    "SET 4,D",      //0xE2
+    "SET 4,E",      //0xE3
+    "SET 4,H",      //0xE4
+    "SET 4,L",      //0xE5
+    "SET 4,(HL)",   //0xE6
+    "SET 4,A",      //0xE7
+    "SET 5,B",      //0xE8
+    "SET 5,C",      //0xE9
+    "SET 5,D",      //0xEA
+    "SET 5,E",      //0xEB
+    "SET 5,H",      //0xEC
+    "SET 5,L",      //0xED
+    "SET 5,(HL)",   //0xEE
+    "SET 5,A",      //0xEF
+    "SET 6,B",      //0xF0
+    "SET 6,C",      //0xF1
+    "SET 6,D",      //0xF2
+    "SET 6,E",      //0xF3
+    "SET 6,H",      //0xF4
+    "SET 6,L",      //0xF5
+    "SET 6,(HL)",   //0xF6
+    "SET 6,A",      //0xF7
+    "SET 7,B",      //0xF8
+    "SET 7,C",      //0xF9
+    "SET 7,D",      //0xFA
+    "SET 7,E",      //0xFB
+    "SET 7,H",      //0xFC
+    "SET 7,L",      //0xFD
+    "SET 7,(HL)",   //0xFE
+    "SET 7,A",      //0xFF
+};
+
+static const unsigned char EXT_INST_LEN = 2;
+
 #endif
